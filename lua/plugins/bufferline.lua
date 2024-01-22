4d8eeed1d17c7b964e359ec1c739439e9c1d264b
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-01-23 00:27:32 +0530

    New configuration awesome

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    keys = {
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    },
+    config = function()
+      vim.opt.termguicolors = true
+      local bufferline = require("bufferline")
+      bufferline.setup{
+        options = {
+          mode = "buffers",
+          style_preset = bufferline.style_preset.default,
+          numbers = "ordinal",
+          buffer_close_icon = '󰅖',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+          diagnostics = "nvim_lsp",
+          separator_style = "slant", 
+          indicator = {
+            style = "underline"
+          },
+        }
+      }
+    end
+  }
+}
