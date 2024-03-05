983d548c6cb24d3db46db21300702fe711a7b205
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-03-05 20:34:58 +0530

    Enhance buffer definitions using telescope

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -15,13 +15,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
 
     -- Lists all the implementations for the symbol under the cursor
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    bufmap('n', 'gi', '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>')
 
     -- Jumps to the definition of the type symbol
     bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
 
     -- Lists all the references 
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+    bufmap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>')
 
     -- Displays a function's signature information
     bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
