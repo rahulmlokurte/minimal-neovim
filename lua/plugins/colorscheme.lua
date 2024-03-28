97c54bbb17960853ec5a7ce78fffd0831eb68c20
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-03-29 00:44:57 +0530

    Add icons

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,16 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      local theme_colors = require("config.colors")
       require("catppuccin").setup({
+        transparent_background = true,
+        color_overrides = { all = theme_colors },
+        custom_highlights = function(colors)
+          return {
+            CurSearch = { bg = colors.green },
+            GitSignsChange = { fg = colors.blue },
+          }
+        end,
         integrations = {
           cmp = true,
           gitsigns = true,
@@ -12,6 +21,8 @@ return {
           treesitter = true,
           dap_ui = true,
           noice = true,
+          lsp_trouble = true,
+          lsp_saga = true,
           native_lsp = {
             enabled = true,
             virtual_text = {
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,43 +1,80 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup {
+  "nvim-lualine/lualine.nvim",
+  enabled = true,
+  event = "VeryLazy",
+  opts = function(plugin)
+    if plugin.override then
+      require("lazyvim.util").deprecate("lualine.override", "lualine.opts")
+    end
+
+    local icons = require("config.icons")
+
+    local diagnostics = {
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      sections = { "error", "warn", "info", "hint" },
+      symbols = {
+        error = icons.diagnostics.Error,
+        hint = icons.diagnostics.Hint,
+        info = icons.diagnostics.Info,
+        warn = icons.diagnostics.Warn,
+      },
+      colored = true,
+      update_in_insert = false,
+      always_visible = false,
+    }
+
+    local diff = {
+      "diff",
+      symbols = {
+        added = icons.git.added .. " ",
+        untracked = icons.git.added .. " ",
+        modified = icons.git.changed .. " ",
+        removed = icons.git.deleted .. " ",
+      },
+      colored = true,
+      always_visible = false,
+      source = function()
+        local gitsigns = vim.b.gitsigns_status_dict
+        if gitsigns then
+          return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed,
+          }
+        end
+      end,
+    }
+
+    return {
       options = {
         theme = "catppuccin",
+        globalstatus = true,
         icons_enabled = true,
         component_separators = '|',
         section_separators = '',
+        disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
       },
       sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-          }
-        },
         lualine_a = {
           {
-            'filename',
-            file_status = true,     -- Displays file status (readonly status, modified status)
-            newfile_status = false, -- Display new file status (new file means no write after created)
-            path = 3,               -- 0: Just the filename
-            -- 1: Relative path
-            -- 2: Absolute path
-            -- 3: Absolute path, with tilde as the home directory
-            -- 4: Filename and parent dir, with tilde as the home directory
-
-            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-            -- for other components. (terrible name, any suggestions?)
+            "filename",
+            path = 0,
+            use_mode_colors = true,
+            shorting_target = 40,
             symbols = {
-              modified = '[+]',      -- Text to show when the file is modified.
-              readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-              unnamed = '[No Name]', -- Text to show for unnamed buffers.
-              newfile = '[New]',     -- Text to show for newly created file before first write
-            }
+              modified = ' ●', -- Text to show when the buffer is modified
+              alternate_file = '#', -- Text to show to identify the alternate file
+              directory = '', -- Text to show when the buffer is a directory
+            },
           }
-        }
-      }
+        },
+        lualine_b = { "branch" },
+        lualine_c = { diagnostics },
+        lualine_x = { diff },
+        lualine_y = { "progress" },
+        lualine_z = {},
+      },
     }
-  end
+  end,
 }
